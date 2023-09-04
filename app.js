d53ae15d23700f71666b7631c352512899b7b0df
Tesfamariam21 <tctesfa21@gmail.com>
2023-09-04 14:52:59 +0300

    updated

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb://localhost/Social_media API',{
+
+}).then(()=>{
+    console.log('Connected to mongodb');
+}).catch(err =>{
+    console.log('Fail to connet:', err);
+});
 
 
 app.listen(3000, ()=>{
--- routes/media.js
+++ routes/media.js
@@ -2,9 +2,68 @@ const express = require('express');
 
 const router = express.Router();
 
-
-router.get('/home',async ()=>{
+//retrive homepage
+router.get('/home',async (req, res)=>{
 
 });
+
+//creating a post
+router.post('/post', async (req, res)=>{
+
+});
+
+//retrive posts
+router.get('/posts', async (req, res)=>{
+
+});
+
+//editing post
+router.put('/post/:post_id', async (req, res)=>{
+
+});
+
+//delete post
+
+router.delete('/post/:post_id', async (req, res)=>{
+
+});
+
+
+//putting a comment for a post
+router.post('/comment', async (req, res)=>{
+
+});
+
+//retrive a given comments 
+router.get('/comments', async (req, res)=>{
+
+});
+
+//edit comment
+router.put('/comment/:com_id', async (req, res)=>{
+
+});
+
+//delete post
+
+router.delete('/comment/:com_id', async (req, res)=>{
+
+});
+
+//follow a user
+router.post('/follow', async (req, res)=>{
+
+});
+
+//retrive followers 
+router.get('/followers', async (req, res)=>{
+
+});
+
+//remove followers and unfollow user
+router.delete('/', async (req, res)=>{
+
+});
+
 
 module.exports = router;
