93e38984346dfc67ef26c0db731ca241d438567a
Tesfamariam21 <tctesfa21@gmail.com>
2023-10-26 17:30:11 +0300

    update

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const ConnectToDB = require('./database.js');
 const authentication = require('./routes/auth');
+const bodyparser = require('body-parser');
 const media = require('./routes/media.js');
 const app = express();
 
 ConnectToDB();
 
+app.use(bodyparser.json());
 app.use(authentication);
-app.use('/media',media);
+app.use('/media', media);
 
 app.listen(3000, ()=>{
     console.log("listening to port 3000");
